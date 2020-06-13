8c44a99c15d681f886bcdff4e92f5e34acbb0bad
Nicholas Chug <nicholas.chug@gmail.com>
2020-06-13 10:41:44 -0400

    removed the getNotes function as it was not being used in the application

--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-    return 'Your notes...';
-}
-
 const addNote = (title, body) => {
     const notes = loadNotes();
     const duplicateNotes = notes.filter((note) => {
@@ -80,7 +76,6 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
